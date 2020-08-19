ac8cee5c720c567c946616664b8787c763d86f2a
Dennis Rieks <dennisrieks@googlemail.com>
2020-08-19 23:55:22 +0200

    example using QApplication.postEvent refs https://github.com/bytedeco/javacpp/issues/419

---
+++ qt/samples/ExampleApplication.java
@@ -0,0 +1,35 @@
+import org.bytedeco.javacpp.IntPointer;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.PointerPointer;
+import org.bytedeco.qt.Qt5Core.QEvent;
+import org.bytedeco.qt.Qt5Widgets.QApplication;
+
+import java.io.File;
+
+public class ExampleApplication {
+    public static void main(String[] args) {
+        String path = Loader.load(org.bytedeco.qt.global.Qt5Core.class);
+        IntPointer argc = new IntPointer(new int[]{3});
+        PointerPointer argv = new PointerPointer("calc", "-platformpluginpath", new File(path).getParent(), null);
+        Application application = new Application(argc, argv);
+        QApplication.postEvent(application, new Event());
+        System.exit(QApplication.exec());
+    }
+
+    public static class Event extends QEvent {
+        public Event() {
+            super(Type.User);
+        }
+    }
+
+    public static class Application extends QApplication {
+        public Application(IntPointer argc, PointerPointer argv) {
+            super(argc, argv);
+        }
+
+        @Override
+        protected void customEvent(QEvent event) {
+            System.out.println("customEvent");
+        }
+    }
+}
