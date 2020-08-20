d18c14cee40e8c313d194d208c1a8b132ac0de51
Dennis Rieks <dennisrieks@googlemail.com>
2020-08-21 00:33:53 +0200

    pointer type bug

--- qt/samples/ExampleApplication.java
+++ qt/samples/ExampleApplication.java
@@ -7,12 +7,14 @@ import org.bytedeco.qt.Qt5Widgets.QApplication;
 import java.io.File;
 
 public class ExampleApplication {
+    private static Event event = new Event();
+
     public static void main(String[] args) {
         String path = Loader.load(org.bytedeco.qt.global.Qt5Core.class);
         IntPointer argc = new IntPointer(new int[]{3});
         PointerPointer argv = new PointerPointer("calc", "-platformpluginpath", new File(path).getParent(), null);
         Application application = new Application(argc, argv);
-        QApplication.postEvent(application, new Event());
+        QApplication.postEvent(application, event);
         System.exit(QApplication.exec());
     }
 
@@ -29,7 +31,8 @@ public class ExampleApplication {
 
         @Override
         protected void customEvent(QEvent event) {
-            System.out.println("customEvent");
+            System.out.printf("global event '%s'\n", ExampleApplication.event);
+            System.out.printf("local event '%s'\n", event);
         }
     }
 }
